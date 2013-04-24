f21939fba5045b51f32fed0aa9178982554337ec
Qing <qgao001@fiu.edu>
2013-04-24 00:44:25 -0400

    only setting first row and column

--- matmul2.cu
+++ matmul2.cu
@@ -6,7 +6,7 @@
 #include "gputimer.h"
 
 #define N 800
-__global__ void MatMul(float d_A[N][N], float d_B[N][N], float d_C[N][N])
+__global__ void MatMul(float **d_A, float d_B[N][N], float d_C[N][N])
 {
   int i = threadIdx.x + blockIdx.x * blockDim.x;   
   int j = threadIdx.y + blockIdx.y * blockDim.y;
@@ -21,15 +21,15 @@ __global__ void MatMul(float d_A[N][N], float d_B[N][N], float d_C[N][N])
   }
 }
 
-__global__ void setElement(float d_A[N][N], float d_B[N][N], float d_C[N][N])
+__global__ void setElement(float d_A[N], float d_B[N], float d_C, int k)
 {
 	int i = threadIdx.x + blockIdx.x * blockDim.x;
-	int j = threadIdx.y + blockIdx.y * blockDim.y;
+	//int j = threadIdx.y + blockIdx.y * blockDim.y;
 
   if (i < N && j < N){
-  	d_A[i][j] = i * (float)3.2 + j * (float)2.21;
-  	d_B[i][j] = i * (float)1.3 + j * (float)3.1;
-    d_C[i][j] = (float)0;
+  	d_A[i] = i * (float)3.2 + k * (float)2.21;
+  	d_B[i] = k * (float)1.3 + i * (float)3.1;
+    d_C[i] = (float)0;
   }
 }
 
@@ -40,30 +40,55 @@ int main()
   //int m,n,k;
   //m = n = k = N;
 
-  //int i,j;
+  int i, j, k;
   int ARRAY_SIZE = N * N;
 
   int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
 
 
   float h_A[N][N], h_B[N][N], h_C[N][N];
-  float (*d_A)[N], (*d_B)[N], (*d_C)[N];
+  float *d_A[N], (*d_B)[N], (*d_C)[N];
 
   cudaMalloc((void**) &d_A, ARRAY_BYTES);
   cudaMalloc((void**) &d_B, ARRAY_BYTES);
-  cudaMalloc((void**) &d_C, ARRAY_BYTES);
+  //cudaMalloc((void**) &d_C, ARRAY_BYTES);
   
   // Kernel invocation with least amount of blocks
   //int numBlocks;
   int block_x = ceil((float)N / (float)512);
-  int block_y = 1; //ceil((float)N / (float)22);
+  int block_y = 1; //single row
+
+  //ceil((float)N / (float)22);
 
   dim3 numBlocks(block_x, block_y);
   
 
   //dim3 threadsPerBlock(22, 22);
+  for (i = 0; i < N; i++)
+  {
+    setElement<<<numBlocks, 512>>>(d_A, d_B, d_C, i); //single row of 512 threads
+    for (j = 0; j < N; j++)
+    {
+      cudaMemcpy(h_A[i][j], d_A[j], sizeof(float), cudaMemcpyDeviceToHost);
+      cudaMemcpy(h_B[i][j], d_B[j], sizeof(float), cudaMemcpyDeviceToHost);
+    }
+  }
 
-  setElement<<<numBlocks, 512>>>(d_A, d_B, d_C)
+  for (i = 0; i < N; i++)
+  {
+    for(j = 0; j < N; j++)
+    {
+      cudaMemcpy(d_A[j], h_A[i][j], sizeof(float), cudaMemcpyHostToDevice);
+      cudaMemcpy(d_B[j], h_B[i][j], sizeof(float), cudaMemcpyHostToDevice);
+    }
+
+    for (j = 0; j < N; j++)
+    {
+      MatMul<<<numBlocks, 512>>>(d_A, d_B, d_C, i);
+    }
+
+  }
+  
 
     /*timer.Start();
   setElement<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C);
@@ -71,9 +96,9 @@ int main()
   timer.Stop();*/
 
 
-  cudaMemcpy(h_A, d_A, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+/*  cudaMemcpy(h_A, d_A, ARRAY_BYTES, cudaMemcpyDeviceToHost);
   cudaMemcpy(h_B, d_B, ARRAY_BYTES, cudaMemcpyDeviceToHost);
-  cudaMemcpy(h_C, d_C, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_C, d_C, ARRAY_BYTES, cudaMemcpyDeviceToHost);*/
   
   
 /*    fprintf(stdout, "Here is the matrix A:\n\n");
